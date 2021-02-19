7d02e226394838732ef4b7de5ef3d8a622f75ec0
Saurabh Sharma <saurabhs@motabilityoperations.co.uk>
2021-02-19 22:08:22 +0000

    Initial code commit-need to fix issues in next commit.

---
+++ src/main/java/com/techsharezone/feignclient/example/FeignClientExampleApplication.java
@@ -0,0 +1,13 @@
+package com.techsharezone.feignclient.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientExampleApplication.class, args);
+	}
+
+}
